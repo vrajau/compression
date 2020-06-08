07c14090f76065cf0d36337a62c4b32ad5897a0a
Agunua <58515278+Agunua@users.noreply.github.com>
2020-06-08 18:57:10 +0200

    Changed code neddddd
    fdsfkdlsjds

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 const options = document.querySelectorAll('.custom-option');
+
 for (const option of options) {
-  option.addEventListener('click', function () {
+  option.addEventListener('click', function (e) {
     Array.from(options).forEach((option) => {
       option.classList.remove('selected');
     });
 
+    if (e.target.classList.contains('custom-option')) {
+      this.classList.add('selected');
 
-    this.classList.add('selected');
-
-    document.querySelector('.custom-select__trigger').innerHTML = '';
-    let selectedSlider = this.cloneNode(true);
-    let arrow = document.createElement('div');
-    arrow.setAttribute('class', 'arrow');
-    arrow.setAttribute('id', 'arrow');
-    selectedSlider.classList.remove('selected');
-    document
-      .querySelector('.custom-select__trigger')
-      .appendChild(selectedSlider);
-    document.querySelector('.custom-select__trigger').appendChild(arrow);
+      document.querySelector('.custom-select__trigger').innerHTML = '';
+      let selectedSlider = this.cloneNode(true);
+      let arrow = document.createElement('div');
+      arrow.setAttribute('class', 'arrow');
+      arrow.setAttribute('id', 'arrow');
+      selectedSlider.classList.remove('selected');
+      document
+        .querySelector('.custom-select__trigger')
+        .appendChild(selectedSlider);
+      document.querySelector('.custom-select__trigger').appendChild(arrow);
+    }
   });
 }
 
-
 /////////////////////////////////////////////////TOGGLE DROPDOWN
-$('#arrowToggle').on('click', function(){
-    $(".custom-select").toggleClass('open');
+$('#arrowToggle').on('click', function () {
+  $('.custom-select').toggleClass('open');
 });
 //------------------------------------------------------------------
 
-
-$('span.custom-option').on('click', function(){
-  const amountOfSpans = $('span.custom-option').length
-  console.log(amountOfSpans)
-  for(i = 0; i < amountOfSpans; i++){
+$('span.custom-option').on('click', function () {
+  const amountOfSpans = $('span.custom-option').length;
+  // console.log(amountOfSpans)
+  for (i = 0; i < amountOfSpans; i++) {
     $('#text0' + i).css('opacity', 0);
-    console.log(i)
+    // console.log(i)
   }
 
   $('#text' + this.id).css('opacity', 1);
-})
+});
 
 /////////////////////////////////////////////////CHANGE THE OPACITY
 window.addEventListener('click', function (e) {
@@ -54,12 +54,9 @@ window.addEventListener('click', function (e) {
     $('#layer3').css('opacity', $(this).val());
   });
 
+  //------------------------------------------------------------------
 
-//------------------------------------------------------------------
-
-
-
-/////////////////////////////////////////////////CLOSE DROPDOWN WHEN CLICKED ON DOCUMENT
+  /////////////////////////////////////////////////CLOSE DROPDOWN WHEN CLICKED ON DOCUMENT
   for (const select of document.querySelectorAll('.custom-select')) {
     if (!select.contains(e.target)) {
       select.classList.remove('open');
