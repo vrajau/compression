f5a3edbd5b4553a0a4368dbe4a0a68a2aa2dcaf0
Agunua <58515278+Agunua@users.noreply.github.com>
2020-06-09 16:56:53 +0200

    images layer 1

--- fade.js
+++ fade.js
@@ -1,9 +1,9 @@
 //add image 1 - 10 (including the 0)
-for(i=1; i < 10; i++){
+for(i=1; i < 401; i++){
     $( "#layer1" ).append( "<div class=tiles class=fadein>" + "<img data-src=img/" + i + ".jpg>" + "</div>");
   }
 
-//add image 10 - 400
-for(i=10; i < 401; i++){
-    $( "#layer1" ).append( "<div class=tiles class=fadein>" + "<img data-src=img/" + i + ".jpg>" + "</div>");
-  }
+// //add image 10 - 400
+// for(i=10; i < 401; i++){
+//     $( "#layer1" ).append( "<div class=tiles class=fadein>" + "<img data-src=img/" + i + ".jpg>" + "</div>");
+//   }
