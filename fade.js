6bb00d1dcbd82cb22e004b2e4e6096822e99d687
Agunua <58515278+Agunua@users.noreply.github.com>
2020-06-09 21:09:27 +0200

    new

--- fade.js
+++ fade.js
@@ -1,9 +1,4 @@
 //add image 1 - 10 (including the 0)
-for(i=1; i < 401; i++){
-    $( "#layer1" ).append( "<div class=tiles class=fadein>" + "<img data-src=img/" + i + ".jpg>" + "</div>");
+for(i=1; i < 169; i++){
+    $( "#layer1" ).append( "<div class=tiles class=fadein>" + "<img data-src=images/" + i + ".jpg>" + "</div>");
   }
-
-// //add image 10 - 400
-// for(i=10; i < 401; i++){
-//     $( "#layer1" ).append( "<div class=tiles class=fadein>" + "<img data-src=img/" + i + ".jpg>" + "</div>");
-//   }
