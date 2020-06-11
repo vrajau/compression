ad410147157b8472909d1e03aa3e0b87b933e718
Agunua <58515278+Agunua@users.noreply.github.com>
2020-06-11 19:31:51 +0200

    filling in

--- dragscroll.js
+++ dragscroll.js
@@ -1,4 +1,29 @@
 (function($) {
+
+    const totalWidth = document.querySelector('.layer-wrapper').scrollWidth;
+    const totalHeight = document.querySelector('.layer-wrapper').scrollHeight; 
+
+    const updateMinimap = function () {
+      const cursorElement = document.querySelector('#minimap__cursor');
+      const boundingBox = document.querySelector('.layer-wrapper').getBoundingClientRect();
+      const minimapElement = document.querySelector('#minimap');
+      const viewportWidth = document.documentElement.clientWidth;
+      const viewportHeight = document.documentElement.clientHeight;
+      const ratioWidth = minimapElement.offsetWidth / totalWidth;
+      const ratioHeight = minimapElement.offsetHeight / totalHeight;
+
+      cursorElement.style.width = `${ratioWidth * viewportWidth - 2}px`;
+      cursorElement.style.height = `${ ratioHeight * viewportHeight - 2}px`;
+      cursorElement.style.top = `${Math.round((-1 * boundingBox.top * ratioHeight))}px`;
+      cursorElement.style.left = `${Math.round((-1 * boundingBox.left * ratioWidth))}px`;
+    };
+
+     
+    // Make sure to update minimap on window resize
+    window.onresize = updateMinimap;
+    window.onload = updateMinimap;  
+    updateMinimap();
+
   $.dragScroll = function(options) {
     var settings = $.extend({
       scrollVertical: true,
@@ -23,41 +48,25 @@
       settings.scrollHorizontal && $el.scrollLeft($el.scrollLeft() + (clickX - e.pageX));
       updateMinimap();
     }
-      const totalWidth = document.querySelector('.layer-wrapper').scrollWidth;
-      const totalHeight = document.querySelector('.layer-wrapper').scrollHeight; 
-    
-      const updateMinimap = function () {
-      const cursorElement = document.querySelector('#minimap__cursor');
-      const boundingBox = document.querySelector('.layer-wrapper').getBoundingClientRect();
-      const minimapElement = document.querySelector('#minimap');
-      const viewportWidth = document.documentElement.clientWidth;
-      const viewportHeight = document.documentElement.clientHeight;
-      const ratioWidth = minimapElement.offsetWidth / totalWidth;
-      const ratioHeight = minimapElement.offsetHeight / totalHeight;
 
-      cursorElement.style.width = `${ratioWidth * viewportWidth - 2}px`;
-      cursorElement.style.height = `${ ratioHeight * viewportHeight - 2}px`;
-      cursorElement.style.top = `${(-1 * boundingBox.top * ratioHeight)}px`;
-      cursorElement.style.left = `${(-1 * boundingBox.left * ratioWidth)}px`;
-    };
-    // Make sure to update minimap on window resize
-    window.onresize = updateMinimap;
-    window.onload = updateMinimap;   
+    function resetMouse() {
+      clicked = false;
+      $('html').css('cursor', 'auto');
+    }
+    
     
 $('#controlarea').on({
       'mousemove': function(e) {
         clicked && updateScrollPos(e, $(document) );
       },
       'mousedown': function(e) {
-        console.log('click')
         clicked = true;
         clickY = e.pageY;
         clickX = e.pageX;
       },
-      'mouseup': function() {
-        clicked = false;
-        $('html').css('cursor', 'auto');
-      }
+
+      mouseup: resetMouse,
+      mouseleave: resetMouse,
     });
   }
 
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ for (const option of options) {
       option.classList.remove('selected');
     });
 
+    $('.selectionText').css('opacity', 0);
+    $(`#text${this.id}`).css('opacity', 1);
+
     if (e.target.classList.contains('custom-option')) {
       this.classList.add('selected');
 
@@ -23,37 +26,84 @@ for (const option of options) {
   });
 }
 
+const sliders = document.querySelectorAll('.slider');
+
+sliders.forEach((slider) => {
+  slider.addEventListener('click', (e) => {
+    e.stopImmediatePropagation();
+  })
+});
+
+
+
+
 /////////////////////////////////////////////////TOGGLE DROPDOWN
 $('#arrowToggle').on('click', function () {
   $('.custom-select').toggleClass('open');
 });
 //------------------------------------------------------------------
 
-$('span.custom-option').on('click', function () {
-  const amountOfSpans = $('span.custom-option').length;
-  // console.log(amountOfSpans)
-  for (i = 0; i < amountOfSpans; i++) {
-    $('#text0' + i).css('opacity', 0);
-    // console.log(i)
-  }
-
-  $('#text' + this.id).css('opacity', 1);
-});
-
 /////////////////////////////////////////////////CHANGE THE OPACITY
 window.addEventListener('click', function (e) {
-  $('#1').on('input', function () {
-    $('#layer1').css('opacity', $(this).val());
+  $('#L15').on('input', function () {
+    $('#layer15').css('opacity', $(this).val());
   });
 
-  $('#2').on('input', function () {
-    $('#layer2').css('opacity', $(this).val());
+  $('#L14').on('input', function () {
+    $('#layer14').css('opacity', $(this).val());
   });
 
-  $('#3').on('input', function () {
+  $('#L13').on('input', function () {
+    $('#layer13').css('opacity', $(this).val());
+  });
+
+  $('#L12').on('input', function () {
+    $('#layer12').css('opacity', $(this).val());
+  });
+
+  $('#L11').on('input', function () {
+    $('#layer11').css('opacity', $(this).val());
+  });
+
+  $('#L10').on('input', function () {
+    $('#layer10').css('opacity', $(this).val());
+  });
+
+  $('#L9').on('input', function () {
+    $('#layer9').css('opacity', $(this).val());
+  });
+
+  $('#L8').on('input', function () {
+    $('#layer8').css('opacity', $(this).val());
+  })
+
+  $('#L7').on('input', function () {
+    $('#layer7').css('opacity', $(this).val());
+  })
+
+  $('#L6').on('input', function () {
+    $('#layer6').css('opacity', $(this).val());
+  })
+
+  $('#L5').on('input', function () {
+    $('#layer5').css('opacity', $(this).val());
+  })
+
+  $('#L4').on('input', function () {
+    $('#layer4').css('opacity', $(this).val());
+  })
+
+  $('#L3').on('input', function () {
     $('#layer3').css('opacity', $(this).val());
-  });
+  })
 
+  $('#L2').on('input', function () {
+    $('#layer2').css('opacity', $(this).val());
+  })
+
+  $('#L1').on('input', function () {
+    $('#layer1').css('opacity', $(this).val());
+  })
   //------------------------------------------------------------------
 
   /////////////////////////////////////////////////CLOSE DROPDOWN WHEN CLICKED ON DOCUMENT
